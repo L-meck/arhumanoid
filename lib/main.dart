d562f5959a936145b9e96443e5684a8c42650162
lameck <lameckkanai3@gmail.com>
2021-03-28 11:13:47 +0300

    false

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Augmented Reality App',
       theme: ThemeData(
         // This is the theme of your application.
         //
