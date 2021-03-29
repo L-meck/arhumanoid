0a4ee292b81744f049bb6daeab7416b8a8392774
lameck <lameckkanai3@gmail.com>
2021-03-29 22:23:23 +0300

    ar 2

--- lib/threeD.dart
+++ lib/threeD.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 class ThreeDSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
-        children: [
-          ListTile(
-            title: Text("3D simulations in your actual living room"),
-          ),
-        ],
+    return SafeArea(
+      child: Container(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Text("3D simulations in your actual living room"),
+            ),
+          ],
+        ),
       ),
     );
   }
